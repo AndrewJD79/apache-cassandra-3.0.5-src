b4a2cfc08ec7ee20430b17e748b48975b7cd7612
Hobin Yoon <hobinyoon@gmail.com>
2016-05-18 17:13:28 +0000

    clean up

--- acorn/test/AcornTest/src/main/java/AcornTest.java
+++ acorn/test/AcornTest/src/main/java/AcornTest.java
@@ -1,5 +1,3 @@
-import java.io.IOException;
-import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
@@ -11,102 +9,6 @@ import com.datastax.driver.core.*;
 
 
 public class AcornTest {
-
-	// TODO: clean up
-//	private static void TestReadAfterWrite()
-//		throws java.lang.InterruptedException, UnknownHostException {
-//		Cass.ExecutionBarrier(_tid);
-//		System.out.printf("TestReadAfterWrite ");
-//		System.out.flush();
-//		int b_tid = _tid;
-//		if (Conf.dc.equals("DC0")) {
-//			while (true) {
-//				Cass.Execute(String.format("INSERT INTO pr_ondemand_client.t0 "
-//							+ "(exp_dt, tid, s_dc, pr_tdcs) "
-//							+ "VALUES ('%s', %d, '%s', {'%s'});",
-//							Conf.dt_begin, _tid, Conf.hns[0], Conf.hns[0]));
-//				ResultSet rs = Cass.Execute(String.format("SELECT * FROM pr_ondemand_client.t0 "
-//							+ "WHERE exp_dt='%s' AND tid=%d;",
-//							Conf.dt_begin, _tid));
-//				int size = rs.all().size();
-//				if (_tid % 100 == 0) {
-//					System.out.printf(".");
-//					System.out.flush();
-//				}
-//				_tid ++;
-//				if (size == 0)
-//					break;
-//			}
-//			System.out.printf(" got 0 in %d pair(s) of INSERT and SELECT.\n", _tid - b_tid);
-//		}
-//	}
-//
-//	private static void _SelectLocalUntil(int tid, int exp_v) {
-//		long bt = System.currentTimeMillis();
-//		System.out.printf("  SelectLocalUntil");
-//		System.out.flush();
-//		while (true) {
-//			int r = Cass.SelectLocal(tid);
-//			System.out.printf(" %d", r);
-//			System.out.flush();
-//			if (r == exp_v)
-//				break;
-//			if (System.currentTimeMillis() - bt > 5000)
-//				throw new RuntimeException("Timed out");
-//		}
-//		System.out.printf("\n");
-//	}
-//
-//	private static void _SelectLocalUntil2(int tid, int exp_v)
-//		throws java.lang.InterruptedException {
-//		long bt = System.currentTimeMillis();
-//		System.out.printf("  SelectLocalUntil");
-//		System.out.flush();
-//		while (true) {
-//			int r = Cass.SelectLocal(tid);
-//			System.out.printf(" %d", r);
-//			System.out.flush();
-//			if (r == exp_v)
-//				break;
-//			if (System.currentTimeMillis() - bt > 5000)
-//				throw new RuntimeException("Timed out");
-//			Thread.sleep(1000);
-//		}
-//		System.out.printf("\n");
-//	}
-//
-//	private static void _SelectLocal(int tid, int exp_v) {
-//		System.out.printf("  SelectLocal ");
-//		System.out.flush();
-//		int r = Cass.SelectLocal(_tid);
-//		System.out.printf("%d\n", r);
-//		if (r != exp_v)
-//			throw new RuntimeException(String.format("got %d. expected %d.", r, exp_v));
-//	}
-//
-//	private static void _SelectAsyncFetchOnDemand(int tid, int exp_v) {
-//		System.out.printf("  SelectAsyncFetchOnDemand ");
-//		System.out.flush();
-//		long bt = System.currentTimeMillis();
-//		int r = Cass.SelectFetchOnDemand(_tid, "mdc-s40", false);
-//		long et = System.currentTimeMillis();
-//		System.out.printf("%d %d ms\n", r, et - bt);
-//		if (r != exp_v)
-//			System.out.printf("    INTERESTING!!! got %d. expected %d.\n", r, exp_v);
-//	}
-//
-//	private static void _SelectSyncFetchOnDemand(int tid, int exp_v) {
-//		System.out.printf("  SelectSyncFetchOnDemand ");
-//		System.out.flush();
-//		long bt = System.currentTimeMillis();
-//		int r = Cass.SelectFetchOnDemand(_tid, "mdc-s40", true);
-//		long et = System.currentTimeMillis();
-//		System.out.printf("%d %d ms\n", r, et - bt);
-//		if (r != exp_v)
-//			throw new RuntimeException(String.format("got %d. expected %d.", r, exp_v));
-//	}
-//
-
 	public static void main(String[] args) throws Exception {
 		try {
 			Conf.ParseArgs(args);
@@ -128,10 +30,6 @@ public class AcornTest {
 			TestTopicFilter();
 			TestReadMakingAttrsPopular();
 			TestFetchOnDemand();
-
-			// TODO: clean up
-			// this takes a really long time. ?? TODO: what is it? Need to remind.
-			//TestReadAfterWrite();
 
 			// Cassandra cluster and session are not closed. Force quit.
 			System.exit(0);
