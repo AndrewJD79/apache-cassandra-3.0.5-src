a33fc458e89c3ac1dfba40e603ac7e36de58ee1f
Hobin Yoon <hobinyoon@gmail.com>
2016-05-19 02:03:26 +0000

    use the public ip of the localhost

--- acorn/clients/youtube/drop-all-acorn-keyspaces.sh
+++ acorn/clients/youtube/drop-all-acorn-keyspaces.sh
@@ -1,5 +1,7 @@
-cqlsh -e "drop keyspace acorn_regular;"
-cqlsh -e "drop keyspace acorn_sync;"
-cqlsh -e "drop keyspace acorn_attr_pop;"
-cqlsh -e "drop keyspace acorn_pr;"
-cqlsh -e "drop keyspace acorn_obj_loc;"
+my_pub_ip=`curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
+
+cqlsh -e "drop keyspace acorn_pr;"       $my_pub_ip
+cqlsh -e "drop keyspace acorn_attr_pop;" $my_pub_ip
+cqlsh -e "drop keyspace acorn_obj_loc;"  $my_pub_ip
+cqlsh -e "drop keyspace acorn_sync;"     $my_pub_ip
+cqlsh -e "drop keyspace acorn_regular;"  $my_pub_ip
