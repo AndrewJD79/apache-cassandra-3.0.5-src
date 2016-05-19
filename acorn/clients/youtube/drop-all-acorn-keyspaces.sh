bfab83b03fdfcbfd5879e2a951c123c407d384cb
Hobin Yoon <hobinyoon@gmail.com>
2016-05-19 04:09:25 +0000

    update

--- acorn/clients/youtube/drop-all-acorn-keyspaces.sh
+++ acorn/clients/youtube/drop-all-acorn-keyspaces.sh
@@ -1,7 +1,8 @@
 my_pub_ip=`curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
 
-cqlsh -e "drop keyspace acorn_pr;"       $my_pub_ip
-cqlsh -e "drop keyspace acorn_attr_pop;" $my_pub_ip
-cqlsh -e "drop keyspace acorn_obj_loc;"  $my_pub_ip
-cqlsh -e "drop keyspace acorn_sync;"     $my_pub_ip
-cqlsh -e "drop keyspace acorn_regular;"  $my_pub_ip
+cqlsh -e "drop keyspace acorn_pr;"          $my_pub_ip
+cqlsh -e "drop keyspace acorn_attr_pop;"    $my_pub_ip
+cqlsh -e "drop keyspace acorn_obj_loc;"     $my_pub_ip
+cqlsh -e "drop keyspace acorn_exe_barrier;" $my_pub_ip
+cqlsh -e "drop keyspace acorn_exp_meta;"    $my_pub_ip
+cqlsh -e "drop keyspace acorn_regular;"     $my_pub_ip
