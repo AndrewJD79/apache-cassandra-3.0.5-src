5d3fb988871c6906eb47775d9f83653cdffeeede
Hobin Yoon <hobinyoon@gmail.com>
2016-05-20 18:30:59 +0000

    update-src-restart-cassandra-except-local-dc

---
+++ acorn/clients/youtube/update-src-restart-cassandra-except-local-dc.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+import datetime
+import os
+import pprint
+import subprocess
+import sys
+import threading
+
+sys.path.insert(0, "/home/ubuntu/work/acorn-tools/util/python")
+import Cons
+import Util
+
+import AcornUtil
+
+
+def GetRemoteDcPubIps():
+	curAz = Util.RunSubp("curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone", print_cmd = False, print_result = False)
+	curRegion = curAz[:-1]
+	#Cons.P(curAz)
+	#Cons.P(curRegion)
+
+	fn = "%s/.run/dc-ip-map" % os.path.dirname(os.path.realpath(__file__))
+	ips = []
+	with open(fn) as fo:
+		for line in fo.readlines():
+			t = line.strip().split(" ")
+			if len(t) != 2:
+				raise RuntimeError("Unexpected format [%s]s" % line)
+			dc = t[0]
+			ip = t[1]
+			if dc != curRegion:
+				ips.append(ip)
+	return ips
+
+
+def RsyncSrcToRemoteDcs():
+	with Cons.MeasureTime("rsync src to remote DCs ..."):
+		remotePubIps = GetRemoteDcPubIps()
+		Cons.P(remotePubIps)
+
+		threads = []
+		for rIp in remotePubIps:
+			t = threading.Thread(target=ThreadRsync, args=[rIp])
+			t.start()
+			threads.append(t)
+
+		for t in threads:
+			t.join()
+
+
+def ThreadRsync(ip):
+	#Cons.P(ip)
+	# Make sure you sync only source files. Syncing build result confuses the
+	# build system.
+	cmd = "cd ~/work/acorn/acorn/clients/youtube" \
+			" && rsync -a -e 'ssh -o \"StrictHostKeyChecking no\" -o \"UserKnownHostsFile /dev/null\"' *.py pom.xml *.yaml src" \
+			" %s:work/acorn/acorn/clients/youtube/" % ip
+	Util.RunSubp(cmd, shell = True, print_cmd = False)
+
+
+# Experiment ID: Current datetime in UTC
+# It is a part of the keyspace name.
+_exp_id = None
+
+
+def main(argv):
+	if len(argv) == 1:
+		_exp_id = datetime.datetime.utcnow().strftime("%y%m%d-%H%M%S")
+	elif len(argv) == 2:
+		_exp_id = argv[1]
+	else:
+		print "Usage: %s" % argv[0]
+		sys.exit(1)
+
+	AcornUtil.GenHostfiles()
+
+	RunPssh("(cd /home/work/acorn" \
+			" && git checkout -- acorn" \
+			" && git pull" \
+			" && ant" \
+			" && cass-restart.py" \
+			")")
+
+
+def RunPssh(cmd):
+	dn_this = os.path.dirname(os.path.realpath(__file__))
+	fn_pssh_hn = "%s/.run/pssh-hostnames-el" % dn_this
+
+	# Stop cassandra. _exp_id is for identifying each run.
+	with Cons.MeasureTime("Running ..."):
+		dn_pssh_out = "%s/.run/pssh-out/%s" % (dn_this, _exp_id)
+		dn_pssh_err = "%s/.run/pssh-err/%s" % (dn_this, _exp_id)
+		Util.RunSubp("mkdir -p %s" % dn_pssh_out)
+		Util.RunSubp("mkdir -p %s" % dn_pssh_err)
+		cmd_pssh = "parallel-ssh -h %s" \
+				" --option=\"StrictHostKeyChecking no\"" \
+				" --option=\"UserKnownHostsFile /dev/null\"" \
+				" -t 0" \
+				" -o %s" \
+				" -e %s" \
+				" %s 2>&1" \
+				% (fn_pssh_hn, dn_pssh_out, dn_pssh_err, cmd)
+		[rc, stdouterr] = _RunSubp(cmd_pssh)
+
+		# Check output with more
+		Util.RunSubp("more %s/*" % dn_pssh_out, shell = True)
+		Util.RunSubp("more %s/*" % dn_pssh_err, shell = True)
+
+		if rc == 0:
+			Cons.P("Success")
+		else:
+			Cons.P("Failure. rc=%d" % rc)
+
+		Cons.P(Util.Indent(stdouterr, 2))
+
+		if rc != 0:
+			sys.exit(0)
+
+
+def _RunSubp(cmd):
+	Cons.P(cmd)
+
+	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	# communidate() waits for termination
+	stdouterr = p.communicate()[0]
+	rc = p.returncode
+	return [rc, stdouterr]
+
+
+if __name__ == "__main__":
+	sys.exit(main(sys.argv))
