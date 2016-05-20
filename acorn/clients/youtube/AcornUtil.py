79a960d80a0f92778d808f0f7e1154331df55f6d
Hobin Yoon <hobinyoon@gmail.com>
2016-05-20 18:28:22 +0000

    gen ip list except the local one

--- acorn/clients/youtube/AcornUtil.py
+++ acorn/clients/youtube/AcornUtil.py
@@ -13,10 +13,14 @@ import DescInst
 def GenHostfiles():
 	dn = "%s/.run" % os.path.dirname(os.path.realpath(__file__))
 	fn_pssh_hn = "%s/pssh-hostnames" % dn
+
+	# Hostnames except local DC
+	fn_pssh_hn_el = "%s/pssh-hostnames-el" % dn
+
 	fn_dc_ip_map = "%s/dc-ip-map" % dn
 
 	# Generate all files if any of them doesn't exist
-	if os.path.isfile(fn_pssh_hn) and os.path.isfile(fn_dc_ip_map):
+	if os.path.isfile(fn_pssh_hn) and os.path.isfile(fn_pssh_hn_el) and os.path.isfile(fn_dc_ip_map):
 		return
 
 	with Cons.MeasureTime("Generating host files ..."):
@@ -34,6 +38,15 @@ def GenHostfiles():
 				fo.write("%s\n" % inst_desc["PublicIpAddress"])
 		Cons.P("Created %s %d" % (fn_pssh_hn, os.path.getsize(fn_pssh_hn)))
 
+		myPubIp = Util.RunSubp("curl -s http://169.254.169.254/latest/meta-data/public-ipv4", print_cmd = False, print_result = False)
+		with open(fn_pssh_hn_el, "w") as fo:
+			for inst_desc in inst_descriptions:
+				if myPubIp == inst_desc["PublicIpAddress"]:
+					continue
+				fo.write("%s\n" % inst_desc["PublicIpAddress"])
+
+		Cons.P("Created %s %d" % (fn_pssh_hn_el, os.path.getsize(fn_pssh_hn_el)))
+
 		with open(fn_dc_ip_map, "w") as fo:
 			for inst_desc in inst_descriptions:
 				az = inst_desc["Placement"]["AvailabilityZone"]
@@ -41,32 +54,3 @@ def GenHostfiles():
 				ip = inst_desc["PublicIpAddress"]
 				fo.write("%s %s\n" % (dc, ip))
 		Cons.P("Created %s %d" % (fn_dc_ip_map, os.path.getsize(fn_dc_ip_map)))
-
-
-#PRJ_ROOT=$HOME/work/pr/2n
-#HOSTS_FILE=$PRJ_ROOT/conf/hosts
-#PSSH_OUT_DIR=/tmp/pssh-out
-#
-#
-#def MergeOutput():
-#	prefix=$1
-#	rm -f $prefix-all
-#	for f in $prefix/*
-#	do
-#		echo "["`basename $f`"]" >> $prefix-all
-#		cat $f >> $prefix-all
-#		echo "" >> $prefix-all
-#	done
-#
-#
-#def CatOutput():
-#	merge_output $@
-#	prefix=$1
-#	cat $prefix-all
-#
-
-#function less_output {
-#	merge_output $@
-#	prefix=$1
-#	less -r $prefix-all
-#}
