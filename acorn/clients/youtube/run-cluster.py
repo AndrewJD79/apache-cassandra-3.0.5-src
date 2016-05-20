ebf6a116742675bd2161496819654da5ad848c76
Hobin Yoon <hobinyoon@gmail.com>
2016-05-20 19:06:41 +0000

    update

--- acorn/clients/youtube/run-cluster.py
+++ acorn/clients/youtube/run-cluster.py
@@ -119,7 +119,7 @@ def main(argv):
 		fo.write("cat .run/pssh-out/%s/* | grep -E \"" \
 				"# writes              :" \
 				"|# reads               :" \
-				"|# fetch on demand(s)  :" \
+				"|# fetch on demand\\(s\\)  :" \
 				"|# read misses - DC loc:" \
 				"|# read misses - Obj   :" \
 				"|# write timeouts      :" \
