da51994a32d1233cc314e76a17bbcd2b7d238dff
Hobin Yoon <hobinyoon@gmail.com>
2016-05-21 01:09:28 +0000

    update

--- acorn/clients/youtube/edit-cassandra-yaml-local.py
+++ acorn/clients/youtube/edit-cassandra-yaml-local.py
@@ -8,16 +8,18 @@ import Util
 
 
 def _EditCassConf():
-	#cmd = "sed -i 's/" \
-	#		"^    use_attr_topic:.*" \
-	#		"/    use_attr_topic: false" \
-	#		"/g' /home/ubuntu/work/acorn/conf/cassandra.yaml"
-
 	cmd = "sed -i 's/" \
 			"^    use_attr_user:.*" \
-			"/    use_attr_user: false" \
+			"/    use_attr_user: true" \
 			"/g' /home/ubuntu/work/acorn/conf/cassandra.yaml"
 	Util.RunSubp(cmd, shell = True)
+
+	cmd = "sed -i 's/" \
+			"^    use_attr_topic:.*" \
+			"/    use_attr_topic: false" \
+			"/g' /home/ubuntu/work/acorn/conf/cassandra.yaml"
+	Util.RunSubp(cmd, shell = True)
+
 
 
 def main(argv):
